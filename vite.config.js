396278d2330de8faff048685386557d218886111
Shivendra sinha <shivendrasinha60@gmail.com>
2025-06-12 13:30:38 +0530

    deployment Script added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ✅ Add this line for GitHub Pages
+const repoName = 'paradise_nursery';
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,  // 👈 important!
 })
